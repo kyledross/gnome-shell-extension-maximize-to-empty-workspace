5b11c94877ea855bb18968459adc7e8794733d6a
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2025-11-15 21:40:39 -0500

    Fixed naming in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-NAME=MaximizeToEmptyWorkspace-gnome49@kyleross.com
+NAME=MaximizeWindowIntoNewWorkspace
 rm -rf ~/.local/share/gnome-shell/extensions/$NAME
 cp -r $NAME ~/.local/share/gnome-shell/extensions/.
